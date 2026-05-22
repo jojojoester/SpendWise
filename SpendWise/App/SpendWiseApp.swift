26e0c1dc89d01547cdae0871393f4f750e60d73b
jojo <aashishrokka3@gmail.com>
2026-05-22 22:16:37 +0545

    added folder structures

---
+++ SpendWise/App/SpendWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendWiseApp.swift
+//  SpendWise
+//
+//  Created by jojo on 22/05/2026.
+//
+//
+//import SwiftUI
+//
+//@main
+//struct SpendWiseApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
